ef3d8cd3bf4937588013a54dc9ee80484808c870
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-05-24 15:52:22 +0200

    create for bike working, but image doesnt for now

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -18,8 +18,14 @@ class BikesController < ApplicationController
   end
 
   def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+    if @bike.save
+      redirect_to bikes_path, notice: 'Bike advert was successfully created.'
+    else
+      render :new
+    end
     authorize @bike
-    redirect_to bikes_path
   end
 
   def edit
