294d676c4fb1c3d9e561ef74f245f989a24e4a2f
Marion Horre <marion.horre@gmail.com>
2022-05-24 11:42:43 +0200

    Controller created, methods in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,8 +3,27 @@ class BookingController < ApplicationController
     @bookings = Booking.all
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def my_bookings
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+
 private
 
-def booking_params
-  params.require(:booking).permit(:user_id, :bike_id, :comment, :start_date, :end_date, :status)
+  def booking_params
+    params.require(:booking).permit(:user_id, :bike_id, :comment, :start_date, :end_date, :status)
+  end
 end
