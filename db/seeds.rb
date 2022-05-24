1de4152efebbfcf8f94d713a311cba73d8dc29e8
Marion Horre <marion.horre@gmail.com>
2022-05-24 10:33:55 +0200

    Seeds - Method create

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Kidbike = {
+Bike.create([{
   name: 'Little pink bike'
   img_url: 'https://m.media-amazon.com/images/I/81E3DzhaKiL._AC_SL1500_.jpg'
   size: 'child'
   category: 'city'
-}
-
-Mountainbike = {
+},
+{
   name: 'VTT Yellow adult'
   img_url: 'https://www.labo-velo.fr/3243-thickbox_default/vtt-trek-procaliber-98-sl-29-jaune-fluo-noir-2018.jpg'
   size: 'adult'
   category: 'mountain'
-}
-
-Electricbike = {
+},
+{
   name: 'Electric Dutch Bike'
   img_url: 'https://cdn.lemond.com/images/pages/dutch/Dutch_Front_3Quarter.webp'
   size: 'adult'
   category: 'electric'
-}
-
-Roadbike = {
+},
+{
   name: 'Road Bike'
   img_url: 'https://images.immediate.co.uk/production/volatile/sites/21/2020/03/Giant-TCR-Advanced-2-Disc-01-64577da.jpg?webp=true&quality=45&resize=1112%2C740'
   size: 'adult'
   category: 'road'
-}
+}])
