6847bcd0c8ec44f1843c29701b296457cb460ecc
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-05-26 16:38:26 +0200

    started stimulus controller for bookings index, dont forget to yarn install

---
+++ app/javascript/controllers/remove_button_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+import { preventOverflow } from "@popperjs/core"
+
+export default class extends Controller {
+  static targets = [ "removed" ]
+
+  connect() {
+    console.log("hello from button_controller!")
+  }
+
+  disappear(event) {
+    console.log(event)
+    event.preventDefault();
+    console.log("hello again");
+  }
+}
